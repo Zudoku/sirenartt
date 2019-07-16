4ad0e4ab23083938ffabfbb114a5dd2a0e6b4a03
Arttu Siren <arttu.siren@gmail.com>
2019-07-17 01:04:21 +0300

    initial codebuild config

--- infra/main.tf
+++ infra/main.tf
@@ -195,3 +195,98 @@ resource "aws_route53_record" "apex" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild_role_policy" {
+  role = "${aws_iam_role.codebuild_role.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateNetworkInterface",
+        "ec2:DescribeDhcpOptions",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DeleteNetworkInterface",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeVpcs"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.www_site.arn}",
+        "${aws_s3_bucket.www_site.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_codebuild_project" "webserver" {
+  name          = "webserver"
+  description   = "aws codebuild project"
+  build_timeout = "5"
+  service_role  = "${aws_iam_role.codebuild_role.arn}"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "nodejs:10"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "BUCKET"
+      value = "${aws_s3_bucket.www_site.bucket}"
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "${var.github_git_url}"
+    git_clone_depth = 1
+  }
+
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,10 +10,6 @@ variable "route_53_zone_id" {
   description = "Route 53 zone id"
 }
 
-variable "aws_access_key" {
-  description = "AWS access key that CodeBuild uses to push static files into the s3 bucket"
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key that CodeBuild uses to push static files into the s3 bucket"
+variable "github_git_url" {
+  description = "Github url for repo: https://github.com/<AUTHOR>/<REPOSITORY>.git"
 }
